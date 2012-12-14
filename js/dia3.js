7469728d60b16d4efb332e9748b844b71cab9120
racs182 <racs182@gmail.com>
2012-12-14 16:03:35 -0430

    Actualizacion de archivos html y js e implementacion de interacciones con css3

---
+++ js/dia3.js
@@ -0,0 +1,33 @@
+// dia3.js
+
+$(document).ready(function() {
+
+  // Aqui debe ir nuestro contenido
+  // $(":range").rangeinput();
+  // $(":date").dateinput(); 
+  // $("#formualariohtml5").validator();
+
+  // if(Modernizr.testAllProps("color")){
+  //   $(":range").rangeinput();Lorem ipsum tempor ex dolore culpa dolor adipisicing magna id eu nulla. 
+  // }
+
+  $('#formualariohtml5').html5form({
+    
+        messages : 'es', // Options 'en', 'es', 'it', 'de', 'fr', 'nl', 'be', 'br'
+        responseDiv : '.errores'
+    
+    })
+
+  /* 
+
+  $(":date").dateinput.localize("es", {
+  months: 'January,February,March,April,May,June,July,August,September,October,November,December',
+  shortMonths:  'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec',
+  days:         'Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
+  shortDays:    'Sun,Mon,Tue,Wed,Thu,Fri,Sat'
+  });
+
+  */
+
+
+});
